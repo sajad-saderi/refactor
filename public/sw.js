8da09d67d0513ef98cffc06f05b268cd7788f6d3
sajad saderi <saderi.sajad@gmail.com>
2020-06-16 08:24:15 +0430

    title change

--- public/sw.js
+++ public/sw.js
@@ -124,10 +124,10 @@ define("./sw.js",['./workbox-1a4a986e'], function (workbox) { 'use strict';
     "revision": "9e99330d7177a9df34074356546c67da"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "bc0430b0cc736957b076b74f9667f74c"
+    "revision": "bf46375c7b848c122697599aebe44d0e"
   }, {
     "url": "/_next/static/runtime/webpack.js.map",
-    "revision": "3e2e517ac0d9e3574089929d9e2efd91"
+    "revision": "282f350601981f5615d06c2c59009cec"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
