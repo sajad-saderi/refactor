60000d6a684e41aa3608c0eca3d9f0a86f7d3b14
sajad saderi <saderi.sajad@gmail.com>
2020-08-18 13:21:13 +0430

    Style reform

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-1a4a986e'], function (workbox) { 'use strict';
     "revision": "1cee59f2903903f6c9f49742c94a5662"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "57d54b9560f118c305e8071903cc83ad"
+    "revision": "efb41f8cdaf86399c7d03d94c3a92f45"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
