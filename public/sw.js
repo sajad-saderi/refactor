e3c063f3971c75f7540d69fc61414a013df37850
sajad saderi <saderi.sajad@gmail.com>
2020-08-25 14:40:12 +0430

    PORT

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-1a4a986e'], function (workbox) { 'use strict';
     "revision": "1cee59f2903903f6c9f49742c94a5662"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "4ebf475c8bf86e2d17ed0c10745cb9dd"
+    "revision": "45119a7012edf7eda16817d2007dbaa3"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
