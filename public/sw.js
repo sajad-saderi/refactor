2db9a0968da17de63cd7047ba68a5af0c0f936b4
sajad saderi <saderi.sajad@gmail.com>
2020-09-05 18:55:36 +0430

    Hiroku Port

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-1a4a986e'], function (workbox) { 'use strict';
     "revision": "1cee59f2903903f6c9f49742c94a5662"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "236876f31a95facf0c218ef9ddc8df19"
+    "revision": "aeb138c67eebb71cb78c2366dca5c5d4"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
