a3b77d3b4ddfe35995f00317da2442a9b0fe6c60
sajad saderi <saderi.sajad@gmail.com>
2020-09-08 16:27:25 +0430

    change the style of the card tags

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-1a4a986e'], function (workbox) { 'use strict';
     "revision": "1cee59f2903903f6c9f49742c94a5662"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "0b86b719a518bf8b65cd8170ea1c9cbc"
+    "revision": "641559ad874c6bf712bfdcb2480b1d46"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
