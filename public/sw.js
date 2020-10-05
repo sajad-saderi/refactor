341fb913d8110232830b942e762406b92b900288
sajad saderi <saderi.sajad@gmail.com>
2020-10-05 13:39:35 +0330

    sme changes in step 2 of add cars

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-45d61607'], function (workbox) { 'use strict';
     "revision": "1cee59f2903903f6c9f49742c94a5662"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "6a7411c6d471c277bfc1a8f6163b7adf"
+    "revision": "e95220a5301f047a48fa7de415863a31"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
