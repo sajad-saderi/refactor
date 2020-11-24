a64fdb12c81de96d4ac7fd557f9bb709d4bf7321
doctor-stranger <sepehrfake@gmail.com>
2020-11-24 13:01:02 +0330

    data layer cehck

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import language from "../public/languages/fa/homepage.json";
 
 const HomePage = () => {
   React.useEffect(() => {
+    console.count("dataLayer");
     window["dataLayer"].push({
       event: "virtualPageView",
       pageURL: window.location.href,
