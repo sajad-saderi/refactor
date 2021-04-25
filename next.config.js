d1880122a02032b7bd12eeb231b9ad59b9e9fa07
doctor-stranger <sepehrfake@gmail.com>
2021-04-25 10:10:59 +0430

    use a new account for the sentry

--- next.config.js
+++ next.config.js
@@ -21,7 +21,8 @@ module.exports = withPWA(
             SITE_URL: "https://sepris.com",
             PRODUCTION_ENDPOINT: "https://core.sepris.com",
             SENTRY:
-              "https://5457324b508844abba775737bc14838e@sentry.io/1547488",
+              "https://ea691a89876c41c6b1e016d8a0f204af@o578542.ingest.sentry.io/5734819",
+            // "https://5457324b508844abba775737bc14838e@sentry.io/1547488",
             GOOGLE_CAPTCHA: "6Ler8sIZAAAAAP_s64xgDM2HCm03wCuARkotJani",
           },
           webpack(config, options) {
