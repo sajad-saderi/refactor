b06e583ba1be0fac4323095f48cb766fd16e4663
doctor-stranger <sepehrfake@gmail.com>
2021-05-18 11:21:12 +0430

    some corrections in cypress tests

--- cypress/integration/join_us/join_us.spec.js
+++ cypress/integration/join_us/join_us.spec.js
@@ -20,33 +20,38 @@ describe("تست های صفحه به ما بپیوندید", () => {
     );
   });
   it("بررسی باکس ماشین حساب", () => {
-    cy.get("[data-test-id=model-name] input")
+    cy.get(
+      ".calculator_dropDown .Disable_Container [data-test-id=drop_down_input]"
+    )
       .should("be.disabled")
       .get(".Blue_BTN.local_Button_joinUs")
       .click()
       .get(".error_Field")
       .should("contain", "سازنده را انتخاب کنید");
-    cy.get("[data-test-id=calculator-form] [data-test-id=brand-name]")
+    cy.get(".calculator_container form .calculator_dropDown:first-child ")
       .click()
       .intercept("GET", core_url + "/car/list?limit=800&brand_id=90")
       .as("get-model-list")
-      .get("[data-test-id=dropbox-search-input]")
+      .get("[data-test-id=search_input]")
       .type("ایران")
       .get("[data-test-id=Items]")
       .eq(0)
       .click()
       .wait("@get-model-list")
       .then(() => {
-        cy.get("[data-test-id=model-name]")
-          .click()
-          .get("[data-test-id=Items]")
-          .eq(0)
+        cy
+          // .get(".calculator_container form .calculator_dropDown:first-child + div")
+          //   .click()
+          //   .get("[data-test-id=Items]")
+          //   .eq(0)
           // .click()
           .get(".Blue_BTN.local_Button_joinUs")
           .click()
           .get(".error_Field")
           .should("contain", "نام مدل را انتخاب کنید")
-          .get("[data-test-id=model-name]")
+          .get(
+            ".calculator_container form .calculator_dropDown:first-child + div"
+          )
           .click()
           .get("[data-test-id=Items]")
           .eq(0)
@@ -55,7 +60,7 @@ describe("تست های صفحه به ما بپیوندید", () => {
           .click()
           .get("[data-test-id=input_error_message]")
           .should("contain", "ارزش خودرو را وارد کنید")
-          .get("[data-test-id=car-price] [data-test-id=input]")
+          .get(".value_container  input")
           .type("۱۰۰۰۰۰۰۰۰");
       });
     cy.intercept("POST", core_url + "/rent-price-estimation-request/new")
@@ -64,52 +69,54 @@ describe("تست های صفحه به ما بپیوندید", () => {
       .click()
       .wait("@rent-price-estimation-request")
       .then(() => {
-        cy.get("[data-test-id=calculator-result-container]")
+        cy.get(".CalculatorResult")
           .should("exist")
-          .get("[data-test-id=calculator-result-container] .eachSvgBox .CalcH3")
+          .get(".CalculatorResult .eachSvgBox .CalcH3")
           .contains("1.1")
-          .get("[data-test-id=addCar_top_joinus_a]")
+          .get(".addCar_top_joinus_a")
           .should("exist")
           .should("have.attr", "href")
           .and("eq", "/login")
-          .get("[data-test-id=go-to-add-car-button]")
+          .get(".add_car_custom.HEAP_joinUs_Btn_AddCar")
           .should("have.attr", "href")
           .and("eq", "/add-car");
       });
   });
   it("بررسی باکس تخمین در سایز 360 پیکسل", () => {
     cy.viewport(360, 660)
-      .get("[data-test-id=brand-name]")
+      .get(".calculator_container form .calculator_dropDown:first-child")
       .should("have.css", "width", "289px")
-      .get("[data-test-id=car-price]")
+      .get(".value_container")
       .should("have.css", "width", "289px")
-      .get("[data-test-id=model-name]")
+      .get(".calculator_container form .calculator_dropDown:first-child + div")
       .should("have.css", "width", "289px")
       .get(".Blue_BTN.local_Button_joinUs")
       .should("have.css", "width", "289px")
-      .get("[data-test-id=calculator-form] [data-test-id=brand-name]")
+      .get(".calculator_container form .calculator_dropDown:first-child")
       .click()
       .intercept("GET", core_url + "/car/list?limit=800&brand_id=90")
       .as("get-model-list-responsive")
-      .get("[data-test-id=dropbox-search-input]")
+      .get("[data-test-id=search_input]")
       .type("ایران")
       .get("[data-test-id=Items]")
       .eq(0)
       .click()
       .wait("@get-model-list-responsive")
       .then(() => {
-        cy.get("[data-test-id=model-name]")
+        cy.get(
+          ".calculator_container form .calculator_dropDown:first-child + div"
+        )
           .click()
           .get("[data-test-id=Items]")
           .eq(0)
           .click()
-          .get("[data-test-id=car-price] [data-test-id=input]")
+          .get(".value_container  input")
           .type("۱۰۰۰۰۰۰۰۰")
           .get(".Blue_BTN.local_Button_joinUs")
           .click()
-          .get("[data-test-id=try-again]")
+          .get(".tryAgainCalc")
           .click()
-          .get("[data-test-id=calculator-result-container]")
+          .get(".CalculatorResult")
           .should("not.exist");
       });
   });
--- cypress/integration/login/login.spec.js
+++ cypress/integration/login/login.spec.js
@@ -6,155 +6,147 @@ const core_url = "https://core.sepris.com/core";
 let home = "http://localhost:3000";
 let search_id = null;
 let car_index_in_list = 0;
-let cell_phone = "9380158835";
+let cell_phone = "09380158835";
 
 describe("بررسی صفحه لاگین", () => {
   beforeEach(() => {
     cy.visit(home);
   });
-  // it("بررسی عملکرد صفحه داینامیک", () => {
-  // cy`.get(".HEAP_Header_Btn_Login a")
-  //   .click()
-  //   .wait(3000)
-  //   .url()
-  //   .should("contain", "/login")
-  //   .go("back")
-  //   .get(".add_car_section .Blue_BTN.add_car_custom")
-  //   .click()
-  //   .wait(6000)
-  //   .url()
-  //   .should("contain", "/login")
-  //   .visit(home)
-  //   .get(".Blue_BTN.search_Btn.HEAP_Home_Btn_Search")
-  //   .click()
-  //   .wait(3000)
-  //   .request(
-  //     "GET",
-  //     core_url +
-  //       `/rental-car/search-for-rent/list?location_id=1&start_date=${date.from_date_form}&end_date=${date.to_date_form}&o=-price&page=1&limit=1`
-  //   )
-  //   .then(() => {
-  //     cy.get(".carCart.HEAP_SearchResult_Card_Car")
-  //       .eq(0)
-  //       .click()
-  //       .wait(3000)
-  //       .get(".Blue_BTN.localClass.HEAP_Car_Btn_Continue")
-  //       .click()
-  //       .wait(3000)
-  //       .get(".Blue_BTN.localClass.HEAP_Checkout_Btn_Book")
-  //       .click()
-  //       .wait(3000)
-  //       .url()
-  //       .should("contain", "/login");
-  //   });`
-  // cy.visit(`${home}/requests`)
-  //   .wait(3000)
-  //   .url("contain", "/login")
-  //   .visit(`${home}/request/12`)
-  //   .wait(3000)
-  //   .url("contain", "/login")
-  //   .visit(`${home}/user/1`)
-  //   .wait(3000)
-  //   .url("contain", "/login");
-  // });
-  // it("بررسی عملکرد باکس لاگین", () => {
-  //   cy.intercept("/add-car")
-  //     .as("go_to_add_car")
-  //     .get(".Blue_BTN.add_car_custom")
-  //     .click()
-  //     .wait("@go_to_add_car")
-  //     .url()
-  //     .should("contain", "/login")
-  //     .get(
-  //       ".Blue_BTN.login_submit.HEAP_ModalGetUserCellPhone_Btn_RequestForConfirmCode "
-  //     )
-  //     .as("submit_form")
-  //     .contains("ارسال کد ورود")
-  //     .get(".input_surround input")
-  //     .as("input")
-  //     .type(12345678999)
-  //     .get("@submit_form")
-  //     .click()
-  //     .get("@input")
-  //     .should("have.class", "inputError")
-  //     .get("@input")
-  //     .clear()
-  //     .type(1234)
-  //     .trigger("blur")
-  //     .get("@input")
-  //     .should("have.class", "inputError")
-  //     .intercept({
-  //       method: "POST",
-  //       url: `${core_url}/device/send-code`,
-  //       form: true,
-  //       body: {
-  //         cell: cell_phone,
-  //       },
-  //     })
-  //     .as("request_for_code")
-  //     .get("@input")
-  //     .clear()
-  //     .type(cell_phone)
-  //     .get("@submit_form")
-  //     .click()
-  //     .wait("@request_for_code")
-  //     .then((result) => {
-  //       expect(result.response.statusCode).to.eq(200);
-  //       cy.get(".login_person_icon")
-  //         .click()
-  //         .get(".login_modal_title h2")
-  //         .contains("ورود / ثبت نام")
-  //         .get("@input")
-  //         .clear()
-  //         .type(cell_phone)
-  //         .get("@submit_form")
-  //         .click()
-  //         .wait(2000)
-  //         .get(".text_input_container label")
-  //         .contains(cell_phone)
-  //         .get(".separated_places .text_input.data-hj-allow")
-  //         .as("separated_input")
-  //         .type(1111)
-  //         .get(".Blue_BTN.login_submit.HEAP_ModalConfirmCode_Btn_Login ")
-  //         .as("submit_login_form")
-  //         .click()
-  //         .get(".border_on_each_Item ")
-  //         .should("have.class", "span_error")
-  //         .get("@separated_input")
-  //         .clear()
-  //         .type(9931)
-  //         .get("@submit_login_form")
-  //         .click()
-  //         .url()
-  //         .should("contain", "/login");
-  //     });
-  // });
+  it("دسترسی بدون لاگین", () => {
+    cy.get(".HEAP_Header_Btn_Login a")
+      .click()
+      .wait(3000)
+      .url()
+      .should("contain", "/login")
+      .go("back")
+      .get(".add_car_section .Blue_BTN.add_car_custom")
+      .click()
+      .wait(6000)
+      .url()
+      .should("contain", "/login")
+      .visit(home)
+      .get(".Blue_BTN.search_Btn.HEAP_Home_Btn_Search")
+      .click()
+      .wait(3000)
+      .request(
+        "GET",
+        core_url +
+          `/rental-car/search-for-rent/list?location_id=1&start_date=${date.from_date_form}&end_date=${date.to_date_form}&o=-price&page=1&limit=1`
+      )
+      .then(() => {
+        cy.get(".carCart.HEAP_SearchResult_Card_Car")
+          .eq(0)
+          .click()
+          .wait(3000)
+          .get(".Blue_BTN.localClass.HEAP_Car_Btn_Continue")
+          .click()
+          .wait(3000)
+          .get(".Blue_BTN.localClass.HEAP_Checkout_Btn_Book")
+          .click()
+          .wait(3000)
+          .url()
+          .should("contain", "/login");
+      });
+    cy.visit(`${home}/requests`)
+      .wait(3000)
+      .url()
+      .should("contain", "/login")
+      .visit(`${home}/request/12`)
+      .wait(3000)
+      .url()
+      .should("contain", "/login");
+  });
+  it("بررسی عملکرد باکس لاگین", () => {
+    cy.get(".Blue_BTN.add_car_custom")
+      .click()
+      .wait(2000)
+      .url()
+      .should("contain", "/login")
+      .get(
+        ".Blue_BTN.login_submit.HEAP_ModalGetUserCellPhone_Btn_RequestForConfirmCode "
+      )
+      .as("submit_form")
+      .contains("ارسال کد ورود")
+      .get(".input_surround input")
+      .as("input")
+      .type(12345678999)
+      .get("@submit_form")
+      .click()
+      .get("@input")
+      .should("have.class", "inputError")
+      .get("@input")
+      .clear()
+      .type(1234)
+      .trigger("blur")
+      .get("@input")
+      .should("have.class", "inputError")
+      .get(".login_person_icon")
+      .click()
+      .get(".login_modal_title h2")
+      .contains("ورود / ثبت نام")
+      .get("@input")
+      .clear()
+      .type(cell_phone)
+      .get("@submit_form")
+      .click()
+      .wait(2000)
+      .request({
+        method: "POST",
+        url: `${core_url}/device/send-code`,
+        form: true,
+        body: {
+          cell: cell_phone,
+        },
+      })
+      .then((result) => {
+        console.log(result);
+        expect(result.status).to.eq(200);
+        cy.get(".text_input_container label")
+          .contains(cell_phone)
+          .get(".separated_places .text_input.data-hj-allow")
+          .as("separated_input")
+          .type(1111)
+          .get(".Blue_BTN.login_submit.HEAP_ModalConfirmCode_Btn_Login ")
+          .as("submit_login_form")
+          .click()
+          .get(".border_on_each_Item ")
+          .should("have.class", "span_error")
+          .get("@separated_input")
+          .clear()
+          .type(9931)
+          .get("@submit_login_form")
+          .click()
+          .url()
+          .should("contain", "/login");
+      });
+  });
   it("بررسی دسترسی های صفحه", () => {
     log_me_in("9380158835").then(() => {
-      cy.get(".Nav> ul")
+      cy.wait(2000)
+        .get(".Nav> ul")
         .children()
         .should("have.length", 3)
-        .intercept("/add-car")
-        .as("go_to_add_car_after_login")
+        // .intercept("/add-car")
+        // .as("go_to_add_car_after_login")
         .get(".Blue_BTN.add_car_custom")
         .click()
-        .wait("@go_to_add_car_after_login")
+        // .wait("@go_to_add_car_after_login")
         .then(() => {
           cy.url()
             .should("contain", "/add-car")
-            .intercept("/requests")
-            .as("request-page")
+            // .intercept("/requests")
+            // .as("request-page")
             .get(".Nav .HEAP_Header_Link_MyOrders")
             .click()
-            .wait("@request-page")
+            // .wait("@request-page")
             .then(() => {
               cy.url()
                 .should("contain", "/requests")
-                .intercept("/user/")
-                .as("profile")
+                // .intercept("/user/")
+                // .as("profile")
                 .get(".first_element_li")
                 .click()
-                .wait("@profile")
+                // .wait("@profile")
                 .then(() => {
                   cy.url()
                     .should("contain", "/user")
