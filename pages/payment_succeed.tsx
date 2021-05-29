e2bf794ca0586c09d0bc8a34d1e49798dc7905f8
doctor-stranger <sepehrfake@gmail.com>
2021-05-29 17:25:12 +0430

    succeed payment

--- pages/payment_succeed.tsx
+++ pages/payment_succeed.tsx
@@ -29,10 +29,7 @@ const Success_payment = () => {
       pageTitle: language.next_seo.title,
     });
     // logPageView();
-    console.log(Router.router.query.id);
-
-    // fetchAPI(Router.router.query.id);
-    fetchAPI(1380);
+    fetchAPI(Router.router.query.id);
   }, []);
 
   /**
@@ -45,15 +42,6 @@ const Success_payment = () => {
       const Order_res: any = await GET_ORDER_REQUEST({ token, id });
       setRent_search_dump(Order_res.data.rent_search_dump);
       setRenter(Order_res.data.renter);
-      console.log(
-        Order_res.data.id,
-        Order_res.data.rent_search_dump.total_price,
-        Order_res.data.rent_search_dump.id,
-        Order_res.data.rent_search_dump.car.name.fa,
-        Order_res.data.rent_search_dump.car.category_set[0].name.fa,
-        Order_res.data.rent_search_dump.avg_price_per_day,
-        Order_res.data.rent_search_dump.no_of_days
-      );
       window["dataLayer"].push({
         event: "purchase",
         transactionId: Order_res.data.id,
