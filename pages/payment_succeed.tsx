95113ee1cc6cb3bc769fc8659b35f91229743af4
doctor-stranger <sepehrfake@gmail.com>
2021-07-03 14:35:52 +0430

    payment_page style

--- pages/payment_succeed.tsx
+++ pages/payment_succeed.tsx
@@ -102,77 +102,85 @@ const Success_payment = () => {
       <article className='responsive minHeight local_payment'>
         {renter ? (
           <section className='payment_cart'>
-            <div className='title margin_top_24 '>
+            <div className='title  '>
               <img src={check_icon} alt='پرداخت موفق' />
               <h4>{language.h4}</h4>
             </div>
             <div className='bank_track_id '>
               <p>شماره پیگیری: {bank_id_track}</p>
             </div>
-            <div className='reserve_information '>
-              <div className='section_title margin_bottom_24'>
-                <img src={paper_check} className='مشخصات خودرو' />
-                <p>مشخصات رزرو</p>
+            <div className='details_section'>
+              <div>
+                <div className='reserve_information '>
+                  <div className='section_title margin_bottom_24'>
+                    <img src={paper_check} className='مشخصات خودرو' />
+                    <p>مشخصات رزرو</p>
+                  </div>
+                  <div className='car_info padding_right_24'>
+                    <div>
+                      <p>خودرو:</p>
+                      <span className='center'>
+                        {rent_search_dump.car.name.fa}
+                      </span>
+                    </div>
+                    <div>
+                      <p>میزبان:</p>
+                      <span className='center'>
+                        {rent_search_dump.owner.company_name
+                          ? rent_search_dump.owner.company_name
+                          : rent_search_dump.owner.name}
+                      </span>
+                    </div>
+                    <div>
+                      <p>تاریخ تحویل:</p>
+                      <span>
+                        {moment(
+                          rent_search_dump.start_date,
+                          "jYYYY/jMM/jDD"
+                        ).format("jYYYY/jMM/jD")}
+                      </span>
+                    </div>
+                    <div>
+                      <p>تاریخ بازگشت:</p>
+                      <span>
+                        {moment(
+                          rent_search_dump.end_date,
+                          "jYYYY/jMM/jDD"
+                        ).format("jYYYY/jMM/jD")}
+                      </span>
+                    </div>
+                    <div>
+                      <p>مدت اجاره:</p>
+                      <span>
+                        {rent_search_dump.no_of_days} {language.day}
+                      </span>
+                    </div>
+                    <div>
+                      <p>محدودیت مسافت:</p>
+                      <span className='float-left'>
+                        {rent_search_dump.max_km_per_day} {language.kilometer}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className='paid_price '>
+                  <p>{language.span_1}</p>
+                  <div>
+                    <h3>{rent_search_dump.total_price.toLocaleString()}</h3>
+                    <span className='toman'> تومان</span>
+                  </div>
+                </div>
               </div>
-              <div className='car_info padding_right_24'>
-                <div>
-                  <p>خودرو:</p>
-                  <span className='center'>{rent_search_dump.car.name.fa}</span>
-                </div>
-                <div>
-                  <p>میزبان:</p>
-                  <span className='center'>
-                    {rent_search_dump.owner.company_name
-                      ? rent_search_dump.owner.company_name
-                      : rent_search_dump.owner.name}
-                  </span>
-                </div>
-                <div>
-                  <p>تاریخ تحویل:</p>
-                  <span>
-                    {moment(
-                      rent_search_dump.start_date,
-                      "jYYYY/jMM/jDD"
-                    ).format("jYYYY/jMM/jD")}
-                  </span>
-                </div>
-                <div>
-                  <p>تاریخ بازگشت:</p>
-                  <span>
-                    {moment(rent_search_dump.end_date, "jYYYY/jMM/jDD").format(
-                      "jYYYY/jMM/jD"
-                    )}
-                  </span>
-                </div>
-                <div>
-                  <p>مدت اجاره:</p>
-                  <span>
-                    {rent_search_dump.no_of_days} {language.day}
-                  </span>
-                </div>
-                <div>
-                  <p>محدودیت مسافت:</p>
-                  <span className='float-left'>
-                    {rent_search_dump.max_km_per_day} {language.kilometer}
-                  </span>
-                </div>
+              <div className='figure_section'>
+                <img
+                  alt={language.car_image}
+                  src={
+                    rent_search_dump.media_set.length > 0
+                      ? rent_search_dump.media_set[0].thumbnail_url
+                      : carImage
+                  }
+                />
               </div>
-              {/* {rent_search_dump.media_set.length > 0 && (
-                // <img
-                //   alt={language.car_image}
-                //   src={
-                //     rent_search_dump.media_set.length > 0
-                //       ? rent_search_dump.media_set[0].thumbnail_url
-                //       : carImage
-                //   }
-                // />
-              )} */}
-            </div>
-            <br />
-            <div className='paid_price '>
-              <p>{language.span_1}</p>
-              <h3>{rent_search_dump.total_price.toLocaleString()}</h3>
-              <span className='toman'> تومان</span>
             </div>
             <div className='delivery_section margin_top_24'>
               <div className='section_title margin_bottom_16'>
@@ -192,7 +200,6 @@ const Success_payment = () => {
                 </p>
               </div>
             </div>
-
             <div className='return_section margin_top_24'>
               <div className='section_title margin_bottom_16'>
                 <img src={return_icon} className='بازگشت خودرو' />
